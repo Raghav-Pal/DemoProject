b1c2f6d016a5f59d8c4e17f33eb6fd5bf8547e21
Raghav-Pal <automation.devops@gmail.com>
2018-08-31 11:29:29 +0530

    Adding demo project

---
+++ src/main/java/test/demo/App.java
@@ -0,0 +1,13 @@
+package test.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "\n Hello World! This is a demo project \n" );
+    }
+}
