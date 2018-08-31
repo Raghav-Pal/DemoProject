170ba83b49d08b68e9ea52932e4033e6f24ac4b1
Raghav-Pal <automation.devops@gmail.com>
2018-08-31 13:01:45 +0530

    updated changes

--- src/main/java/test/demo/App.java
+++ src/main/java/test/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "\nHello World! This is a demo project\n" );
+        System.out.println( " \nHello World! This is a demo project\n" );
     }
 }
